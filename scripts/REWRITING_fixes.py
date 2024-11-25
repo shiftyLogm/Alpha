932a64bb3c0005ebb927102b442332830a18196c
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-11-25 14:56:37 -0300

    Update REWRITING_fixes.py

--- scripts/REWRITING_fixes.py
+++ scripts/REWRITING_fixes.py
@@ -4,6 +4,11 @@ import os
 def verifyFile(m_path) -> bool:
     return os.path.isfile(m_path)
 
-# Increase a number to tile video:
-def increaseTitle(m_path, file_n) -> str:
+# Increase a number to title video:
+def increaseTitle(m_path, v_name, format) -> str:
     
+    i = 1
+    while os.path.exists(m_path + f"/{v_name}({i})" + f".{format}"):
+        i += 1
+
+    return f"{v_name}({i})"
