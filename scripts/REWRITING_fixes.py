b58fc00b3dfcf132739861660509f504ed117d87
Lucas "shiftyLogm" Queiroz <106892560+shiftyLogm@users.noreply.github.com>
2024-11-26 10:20:15 -0300

    Update REWRITING_fixes.py

--- scripts/REWRITING_fixes.py
+++ scripts/REWRITING_fixes.py
@@ -2,7 +2,7 @@ import os
 
 # Check existing file
 def verifyFile(m_path) -> bool:
-    return os.path.isfile(m_path)
+    return os.path.exists(m_path)
 
 # Increase a number to title video:
 def increaseTitle(m_path, v_name, format) -> str:
